d816e86f02008b022b474b91bcf64457edbf8dff
Chris Green <chrisgreen1993@gmail.com>
2025-08-11 11:49:34 +0700

    Update scripts and run biome check

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "bun:test";
-import { generateSwiftRecords, generateKotlinRecords } from "./index";
+import { generateKotlinRecords, generateSwiftRecords } from "./index";
 
 describe("Code generation functions", () => {
 	const sampleTypeScript = `
